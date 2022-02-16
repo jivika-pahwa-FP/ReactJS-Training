415be98c99616a40f2d1d1b6459ac7e939cd17b2
Jivika Pahwa <jivika.pahwa@fareportal.com>
2022-02-16 22:02:12 +0530

    second assignment

---
+++ DomFile.js
@@ -0,0 +1,18 @@
+let box1 = document.getElementById("box1");
+let box2 = document.getElementById("box2");
+
+box1.onmouseover = function(){
+    box1.style.backgroundColor = "crimson";
+};
+
+box1.onmouseout = function(){
+    box1.style.backgroundColor = "Black";
+};
+
+box2.onmouseover = function(){
+    box2.style.backgroundColor = "crimson";
+}
+
+box2.onmouseout = function(){
+    box2.style.backgroundColor = "black";
+}
