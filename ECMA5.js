9fa1635c3204691a00097e53038e2c7653fa6f59
Jivika Pahwa <jivika.pahwa@fareportal.com>
2022-02-22 21:11:15 +0530

    previous assignments

--- ECMA5.js
+++
@@ -1,8 +0,0 @@
-let arr = [2,4,6,8,10,12,14,16,18,20];
-
-const filteredArray = arr.filter((ele,index,arr)=>{
-        return ele > 15;
-});
-
-console.log(filteredArray); // creates a new array
-console.log(arr);
