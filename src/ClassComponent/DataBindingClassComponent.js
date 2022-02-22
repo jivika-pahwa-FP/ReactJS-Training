65e6bca58e5761459538ad0c6f2257e22417c6fd
Jivika Pahwa <jivika.pahwa@fareportal.com>
2022-02-22 21:21:56 +0530

    fourth react assignment

--- src/ClassComponent/DataBindingClassComponent.js
+++ src/ClassComponent/DataBindingClassComponent.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import ChildComponent from "./childComponent";
+import ChildComponent from "./ChildComponent";
 
 export default class DataBindingClassComponent extends Component{
     constructor(){
